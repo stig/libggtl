6c47ae43c70225a2302d362698d8b719f051da30
sbrautaset <sbrautaset@f2cb6906-2018-11de-b650-e715bd6d7cf1>
2009-04-03 06:46:03 +0000

    r1765@russet (orig r1764):  stig | 2006-03-19 22:09:26 +0000  bumping library version number + adding to changelog

--- trunk/ggtl/makefile.mk
+++ trunk/ggtl/makefile.mk
@@ -1,15 +1,17 @@
 lib_LTLIBRARIES         = libggtl.la libnim.la libreversi.la
+ggtl_LDFLAGS            = -no-undefined -version-info 1:0:1
+
 
 libggtl_la_SOURCES      = ggtl/ggtl.c ggtl/ggtlai.c ggtl/private.h
-libggtl_la_LDFLAGS      = -no-undefined -version-info 0:0:0
+libggtl_la_LDFLAGS      = $(ggtl_LDFLAGS)
 
 libnim_la_SOURCES       = ggtl/nim.c
 libnim_la_LIBADD        = libggtl.la
-libnim_la_LDFLAGS       = -no-undefined -version-info 0:0:0
+libnim_la_LDFLAGS       = $(ggtl_LDFLAGS)
 
 libreversi_la_SOURCES   = ggtl/reversi.c
 libreversi_la_LIBADD    = libggtl.la
-libreversi_la_LDFLAGS   = -no-undefined -version-info 0:0:0
+libreversi_la_LDFLAGS   = $(ggtl_LDFLAGS)
 
 nobase_include_HEADERS  = ggtl/core.h \
                           ggtl/nim.h \
