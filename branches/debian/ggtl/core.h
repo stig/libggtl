e91fbb309e9013ab45a54026c74981f4e50b6a7f
sbrautaset <sbrautaset@f2cb6906-2018-11de-b650-e715bd6d7cf1>
2009-04-03 06:49:23 +0000

    r1827@russet (orig r1826):  stig | 2006-04-15 20:11:12 +0100  merge changes from trunk

--- branches/debian/ggtl/core.h
+++ branches/debian/ggtl/core.h
@@ -1,12 +1,24 @@
 /*
- * GGTL - 2-player strategic games AI
- * Copyright (C) 2005-2006 Stig Brautaset
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- */
+GGTL - 2-player strategic games AI.
+Copyright (C) 2005-2006 Stig Brautaset. All rights reserved.
+
+This file is part of GGTL.
+
+GGTL is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+GGTL is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GGTL; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
 
 #ifndef ggtl__core_h
 #define ggtl__core_h
