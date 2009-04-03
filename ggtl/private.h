e03b5422dad616f07ce13c6bccbbfb3d91af05bd
sbrautaset <sbrautaset@f2cb6906-2018-11de-b650-e715bd6d7cf1>
2009-04-03 06:48:00 +0000

    r1775@russet (orig r1774):  stig | 2006-03-26 22:38:09 +0100  dropped an internal typedef

--- ggtl/private.h
+++ ggtl/private.h
@@ -3,8 +3,6 @@
 
 #define GGTL_ERR (GGTL_FITNESS_MAX+1)
 
-typedef double ggtl_time_t;
-
 struct ggtl {
   GGTL_VTAB *vtab;
 
@@ -17,7 +15,7 @@ struct ggtl {
 
   /* run-time options */
   int opts[GET_KEYS];
-  ggtl_time_t time_to_search;
+  float time_to_search;
 
   /* optimisation for end of search */
   int saw_end;
